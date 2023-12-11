d90fd13d81999a7aa2863a7c1622cc3a6925d31b
JiangYiwen0 <147826660+JiangYiwen0@users.noreply.github.com>
2023-12-11 16:51:34 +0800

    Almost done

--- MovieProject/src/Driver.java
+++ MovieProject/src/Driver.java
@@ -19,7 +19,7 @@ public class Driver{
               2)List all the movies as you want
               3)Find a Movie
               4）Delete a Movie
-              5)Partial match search
+              5)Update a Movie
               0)Exit 
               """);
       int option = input.nextInt();
@@ -35,71 +35,120 @@ public class Driver{
               case 2 -> printAllMovies();
               case 3 -> findAMovie();
               case 4 -> deleteAMovie();
-              case 5 -> partialSearch();
-              default -> System.out.println("Invaild option entered" + option);
+              case 5 -> updateMovie();
+              default -> System.out.println("Invaild option entered,please input again");
 
           }
-          System.out.println("\nPress enter key to continue");
-          input.nextLine();
-          input.nextLine();
-          input.nextLine();
-
           option = mainMeun();
       }
       System.out.println("EXIT.Hope you have a nice day!");
       System.exit(0);
     }
-
     private void setup() {
         System.out.println("How many movies do you want to add?");
         int numberMovies = input.nextInt();
         Library library = new Library(numberMovies);
     }
-    private void addAMovie() {
-      input.nextLine();
-      System.out.print("Enter the Movie name:");
-      String movieName= input.nextLine();
-      System.out.print(" Enter the release year:");
-      int releaseYear = input.nextInt();
-      System.out.println("Please enter the initials of the director's name:");
-      String director = input.nextLine();
-      System.out.print("Enter the ranking of this movie:");
-      int ranking = input.nextInt();
-      System.out.println("Is this movie win an Oscar?Yes or No?:");
-      String oscarOrNot = input.nextLine();
 
-      Movie film = new Movie(movieName,releaseYear,director,ranking,oscarOrNot);
-      boolean addedIn =Library.add(film);
-      if (addedIn){
-          System.out.println("You have added a movie successfully in the Movie Library!");
-        }
-      else {
+    /**
+     * 添加电影
+     */
+    private void addAMovie() {
+        System.out.println("Enter the Movie id:");
+        int id = input.nextInt();
+        input.nextLine();
+        System.out.println("Enter the Movie name:");
+//        input.nextLine();
+        String movieName = input.nextLine();
+        System.out.println("Enter the release year:");
+        int releaseYear = input.nextInt();
+        input.nextLine();//清除缓冲区中的换行符
+        System.out.println("Please enter the initials of the director's name:");
+        String director = input.nextLine();
+        System.out.println("Enter the ranking of this movie:");
+        int ranking = input.nextInt();
+        input.nextLine();//清除缓冲区中的换行符
+        System.out.println("Is this movie win an Oscar?Yes or No?:");
+        String oscarOrNot = input.nextLine();
+
+        Movie film = new Movie(id, movieName, releaseYear, director, ranking, oscarOrNot);
+        boolean addedIn = Library.add(film);
+        if (addedIn) {
+            System.out.println(" You have added the movie:\n " +
+                    film.toString() + "\n" +
+                    " successfully in the Movie Library!");
+        } else {
             System.out.println("No movie added in!");
         }
     }
+
+    /**
+     * 打印所有电影
+     */
     private void printAllMovies() {
         System.out.println("The Movie Library is:");
         System.out.println(Library.list());
     }
+
+    /**
+     * 查找单个电影 根据电影名
+     */
     private void findAMovie() {
-      input.nextLine();
-      System.out.print("Please enter a movie name you favor:");
-      String movieName = input.nextLine();
-      Movie findAMovie = Library.find(movieName);
+        input.nextLine();
+        System.out.print("Please enter a movie name you favor:");
+        String movieName = input.nextLine();
+        Movie findAMovie = Library.find(movieName);
 
-      if (findAMovie != null){
-          System.out.println("What i find is"+ findAMovie);
-      }
-      else {
-          System.out.println("Sorry,i can't find the ["+ findAMovie +"] in the Movie library");
-      }
+        if (findAMovie != null) {
+            System.out.println("What i find is-->" + findAMovie);
+        } else {
+            System.out.println("Sorry,i can't find the [movie] in the Movie library");
+        }
     }
 
+    /**
+     * 删除电影 根据电影id
+     */
     private void deleteAMovie() {
-      System.out.println("What do you want to delete:");
-
+        input.nextLine();
+        System.out.println("What do you want to delete(by id):");
+        int id = input.nextInt();
+        int result = Library.delete(id);
+        if (result == 0) {
+            System.out.println("delete fail!");
+        }
+        if (result == 1) {
+            System.out.println("delete success!");
+        }
     }
-    private void partialSearch() {
-        String m = input.nextLine();
+
+    /**
+     * 更新电影数据
+     */
+    private void updateMovie() {
+        System.out.println("Enter the Movie id:");
+        int id = input.nextInt();
+        input.nextLine();
+        System.out.println("Enter the Movie name:");
+//        input.nextLine();
+        String movieName = input.nextLine();
+        System.out.println("Enter the release year:");
+        int releaseYear = input.nextInt();
+        input.nextLine();//清除缓冲区中的换行符
+        System.out.println("Please enter the initials of the director's name:");
+        String director = input.nextLine();
+        System.out.println("Enter the ranking of this movie:");
+        int ranking = input.nextInt();
+        input.nextLine();//清除缓冲区中的换行符
+        System.out.println("Is this movie win an Oscar?Yes or No?:");
+        String oscarOrNot = input.nextLine();
+        Movie film = new Movie(id, movieName, releaseYear, director, ranking, oscarOrNot);
+        int result= Library.update(film);
+        if (result == 0) {
+            System.out.println("update fail!");
+        }
+        if (result == 1) {
+            System.out.println("update success!");
+        }
     }
 }
--- MovieProject/src/Library.java
+++ MovieProject/src/Library.java
@@ -29,7 +29,7 @@ public class Library {
         else {
             String listOfMovies ="";
             for (int i = 0;i<total;i++){
-                listOfMovies += i +":" + movies[i] + "\n";
+                listOfMovies += "Movie"+i+1 +":" + movies[i].toString()+ "\n";
             }
             return listOfMovies;
         }
@@ -47,5 +47,44 @@ public class Library {
             return null;
         }
         return foundMovie;
+    }
+    public static int delete(int id){
+        int index = -1;
+        for (int i = 0; i < movies.length; i++) {
+            if (movies[i].getId()==id) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            Movie[] newArray = new Movie[movies.length - 1];
+            for (int i = 0, j = 0; i < movies.length; i++) {
+                if (i != index) {
+                    newArray[j++] = movies[i];
+                }
+            }
+            for(int i = 0;i< movies.length-1;i++){
+                movies[i] = newArray[i];
+            }
+            total-=1;
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    public static int update(Movie movie){
+            int index = -1;
+            for (int i = 0; i < movies.length; i++) {
+                if (movies[i].getId()==movie.getId()) {
+                    index = i;
+                    break;
+                }
+            }
+            movies[index].setMovieName(movie.getMovieName());
+            movies[index].setDirector(movie.getDirector());
+            movies[index].setRanking(movie.getRanking());
+            movies[index].setOscarOrNot(movie.getOscarOrNot());
+            movies[index].setReleaseYear(movie.getReleaseYear());
+            return 1;
     }
 }
